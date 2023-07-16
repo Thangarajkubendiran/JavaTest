0d17d537eaee0fc849d0325b0b6e1a15a600454e
Thangarajk <thankarajbecse@gmail.com>
2023-07-16 10:13:19 +0530

    Test commit

--- src/com/functionalinterface/CalculatorImpl.java
+++ src/com/functionalinterface/CalculatorImpl.java
@@ -5,7 +5,7 @@ public class CalculatorImpl{
     public static void main(String[] args) {
         IntStream.rangeClosed(1,100).mapToObj(i->i%5==0?(i%7==0?"FizzBuzz":"Fizz"):(i%7==0?"Buzz":i))
         .forEach(System.out::println);
-        System.out.println("Test");
+        System.out.println("Testing");
     }
 
 }
